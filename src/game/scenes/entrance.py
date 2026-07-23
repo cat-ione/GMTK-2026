5e82a419f53e21fb57c341334bf1efd2289ceb26
cat-ione <ionewang@catione.dev>
2026-07-23 18:19:37 -0400

    add template other rooms

---
+++ src/game/scenes/entrance.py
@@ -0,0 +1,11 @@
+from src.core import *
+
+from .room import Room
+from src.game.sprites.item import Vacuum
+from src.game.sprites.dust import Dust
+
+class Entrance(Room):
+    def __init__(self, game: Game) -> None:
+        super().__init__(game, "entrance")
+
+        self.set_boundary([(0, 29), (139, 29), (139, 139), (65, 139), (65, 86), (0, 86)])
