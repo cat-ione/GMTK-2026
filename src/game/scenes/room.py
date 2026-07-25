cf9b1657889d9b89c8a44f86d9c69a6d803a0406
cat-ione <ionewang@catione.dev>
2026-07-25 19:05:59 -0400

    move clipboard icon

--- src/game/scenes/room.py
+++ src/game/scenes/room.py
@@ -103,7 +103,7 @@ class Room(Scene):
 
         if not self.clipboard_visible and self.cutscene is None:
             img = Image.get("clipboard_icon")
-            screen.blit(img, (4, HEIGHT - img.height - 4))
+            screen.blit(img, (WIDTH - img.width - 4, 4))
 
     def set_interactable_furniture(self, d: dict[str, type[Furniture]]) -> None:
         self.interactable_furniture = d
