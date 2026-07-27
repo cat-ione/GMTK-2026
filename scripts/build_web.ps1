39dc3f52b8d30263da7f1eb12f8c0306368cfdc7
cat-ione <ionewang@catione.dev>
2026-07-26 23:00:58 -0400

    fix web build

--- scripts/build_web.ps1
+++ scripts/build_web.ps1
@@ -3,9 +3,9 @@ param(
 )
 
 uv run scripts\web_prebuild.py . build\web -OO
-pygbag --PYBUILD 3.14 --can_close 1 --archive build\web\main.py
+pygbag --PYBUILD 3.14 --can_close 1 --disable-sound-format-error --archive build\web\main.py
 cp build\web\build\web.zip dist
 
 if ($r) {
-    pygbag --PYBUILD 3.14 --can_close 1 build\web\main.py
+    pygbag --PYBUILD 3.14 --can_close 1 --disable-sound-format-error build\web\main.py
 }
